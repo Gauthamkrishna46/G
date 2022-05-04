519a7e8ac19ed129428a6b4115d3b1d1e88e218f
Gauthamkrishna46 <104895536+Gauthamkrishna46@users.noreply.github.com>
2022-05-04 21:55:10 +0530

    Arithmetic

--- SAP.sh
+++ SAP.sh
@@ -1,8 +1,8 @@
 echo "Enter Three Numbers: "
-read a b c
-s=`expr $a + $b + $c`
-a=`expr $s / 3`
-p=`expr $a * $b * $c`
-echo "Sum: $s"
-echo "Average: $a"
-echo "Product: $p"
+read a b
+s=`expr $a + $b`
+d=`expr $a - $b`
+p=`expr $a \* $b`
+q=`expr $a % $b`
+echo $s $d $p $q
+
